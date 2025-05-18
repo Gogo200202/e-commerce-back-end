a49058af816c0fc9330cfdb33036a1f4ce8f9994
Gogo200202 <gosheto420@gmail.com>
2025-05-18 22:35:13 +0300

    add items by name

--- database/ItemsQuery.js
+++ database/ItemsQuery.js
@@ -1,26 +1,37 @@
 const { Db } = require("./connectionDb");
 
-var ObjectID = require('mongodb').ObjectID;
-var mongo = require('mongodb');
-
+var ObjectID = require("mongodb").ObjectID;
+var mongo = require("mongodb");
 
 async function GetAllItems() {
   let DbConnection = await Db;
 
-  let Items = await DbConnection.Items.find().toArray();
+  const Items = await DbConnection.Items.find().toArray();
 
-   return Items;
+  return Items;
 }
-
 
 async function GetItemById(id) {
   let DbConnection = await Db;
 
+  const Item = await DbConnection.Items.findOne({
+    _id: new mongo.ObjectId(id),
+  });
 
-  let Item = await DbConnection.Items.findOne({_id:new mongo.ObjectId(id)});
-    console.log(Item)
-   return Item;
+  return Item;
 }
 
+async function GetItemsByName(name) {
+  let DbConnection = await Db;
 
-module.exports = { GetAllItems, GetItemById}
+  let obj = {
+    name: new RegExp(name, "i"),
+  };
+
+
+  const Items = await DbConnection.Items.find(obj).toArray();
+
+  return Items;
+}
+
+module.exports = { GetAllItems, GetItemById, GetItemsByName };
--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const port = 8080;
 app.use(express.json());
 app.use(cors());
 
-const { GetAllItems, GetItemById } = require("./database/ItemsQuery");
+const {
+  GetAllItems,
+  GetItemById,
+  GetItemsByName,
+} = require("./database/ItemsQuery");
 
 app.get("/", async function (req, res) {
   res.json({ Message: "not here" });
@@ -19,13 +23,19 @@ app.get("/getAllItems", async function (req, res, next) {
 });
 
 app.post("/getById", async function (req, res) {
-  console.log(req.body);
-
   let id = req.body._id;
 
   let item = await GetItemById(id);
 
   res.json({ item });
+});
+
+app.post("/getByName", async function (req, res) {
+  let name = req.body.name;
+
+  let items = await GetItemsByName(name);
+  
+  res.json({ items: items });
 });
 
 app.listen(port, function (err) {
