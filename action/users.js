31f0dd4747ce76f4e5d872f4b55b3dc899332538
Gogo200202 <gosheto420@gmail.com>
2025-06-04 12:32:49 +0300

    add SingIn and SingUp

--- action/users.js
+++ action/users.js
@@ -2,8 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 const jwt = require("jsonwebtoken");
 const { Router, request } = require("express");
-const { validateTokenMiddleware } = require("../middleware/jwt");
-const { CreateUser } = require("../database/user");
+
+const { CreateUser, fiendByUserName } = require("../database/user");
 const { generateTokenFunction } = require("../utils/jwt");
 const { hashPassword, checkPassword } = require("../utils/hashPasswords");
 
@@ -13,20 +13,33 @@ const bcrypt = require("bcrypt");
 
 router.post("/user/CreateUser", async (req, res) => {
   let body = req.body;
-
   let hashedPassword = await hashPassword(body["password"]);
   body["password"] = hashedPassword;
-   let newUser = await CreateUser(body);
- 
-  let jwt = generateTokenFunction(body["userName"], newUser.insertedId);
-  res.json({ "Jwt": jwt });
+
+  let user = await fiendByUserName(body.userName);
+
+  if (user == null) {
+    
+    let newUser = await CreateUser(body);
+    let jwt = generateTokenFunction(body["userName"], newUser.insertedId);
+    res.status(200).json({ Jwt: jwt });
+  } else {
+    res.status(401).json({ Message: "This user name is already taken" });
+  }
 });
 
-router.post("/user/login", validateTokenMiddleware, (req, res) => {
-  if (res.locals.jwtValid) {
-    res.json({ User: "login" });
+router.post("/user/login", async (req, res) => {
+  let body = req.body;
+  let user = await fiendByUserName(body.userName);
+  if (user != null) {
+    if (await checkPassword(body["password"], user.password)) {
+      let jwt = generateTokenFunction(body["userName"], user.insertedId);
+      res.status(200).json({ Jwt: jwt });
+    } else {
+      res.status(401).json({ Message: "not valid password or user name" });
+    }
   } else {
-    res.json({ User: "not login" });
+    res.status(401).json({ Message: "not valid password or user name" });
   }
 });
 
--- database/user.js
+++ database/user.js
@@ -1,3 +1,5 @@
+
+
 const { Db } = require("./connectionDb");
 
 var ObjectID = require("mongodb").ObjectID;
@@ -5,10 +7,26 @@ var mongo = require("mongodb");
 
 async function CreateUser(newUser) {
   let DbConnection = await Db;
+  let newUserObject={
+    userName:newUser.userName,
+    password:newUser.password,
+    email:newUser.email,
+    likedProducts:[],
+    publishedProducts:[]
+  }
 
-  const user = await DbConnection.Users.insertOne(newUser);
+  const user = await DbConnection.Users.insertOne(newUserObject);
 
   return user;
 }
 
-module.exports = { CreateUser };
+
+async function fiendByUserName(userName) {
+  let DbConnection = await Db;
+
+  const user = await DbConnection.Users.findOne({"userName":userName});
+
+  return user;
+}
+
+module.exports = { CreateUser,fiendByUserName };
